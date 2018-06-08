5c5a44f40a39491689bdf63a7b6b709d2d8673c1
Nickolas Gough <nickolas.v.gough@gmail.com>
2018-06-07 21:32:32 -0600

    Generate sidenav.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgSideNavComponent } from './ng-side-nav/ng-side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgSideNavComponent
   ],
   imports: [
     BrowserModule
